57b444a178abb9ac51778d3a938eef2e85ca0312
muhammadibrahim-dev <muhammadibrahimmughal35@gmail.com>
2025-03-27 21:36:12 +0500

    todoapp complete

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,41 @@ function App() {
 const [todo,setTodo]=useState([
 
 ])
+//-----------------------------edittodo-----------------------------
+const [editid,setEditid]=useState(null)
 // --------------------------Add Todo Function----------------------- 
 const addTodohandle =()=>{
   //------------------------Empty_input_stop_add---------------------
   if(addtodo !== ''){
-      const newTodo = {
+      if(editid){
+    //----------------updata todos-------------------------------
+    setTodo(todo.map((item)=> item.id === editid ? {...item, text: addtodo}:item));
+    setEditid(null)
+    setAddtodo('')
+}else{
+  const newTodo = {
     id: todo.length + 1,
     text: addtodo,
   }
-  //------------------combine_new_and_old_data------------------------
-  setTodo([...todo,newTodo]);
-  //------------------after_add_todo_input_empty-----------------------
-setAddtodo('')
-}else{
-  alert('Please Enter name')
+ 
+
+   //------------------combine_new_and_old_data------------------------
+   setTodo([...todo,newTodo]);
+   setAddtodo('')
+  }}else{
+    alert('Enter user text')
+  } 
 }
-  }
 //-------------------Delete_Todo_function----------------------------
 const DeleteTodo =(id)=>{
 setTodo(todo.filter((v)=>v.id !== id));
 };
 //----------------Edit_Todo_funcion------------------------------------
-
+const EditTodo=(id)=>{
+  const todoEdit = todo.find((item)=> item.id === id);
+  setAddtodo(todoEdit.text);
+  setEditid(id);
+}
   return (
     <>
     
@@ -38,8 +51,8 @@ setTodo(todo.filter((v)=>v.id !== id));
       <div className="border p-10 text-center">
         <h1 className="text-3xl font-bold ">Todo List</h1>
         <div>
-          <input type="text" value={addtodo} onChange={(e)=>setAddtodo(e.target.value)} placeholder="Enter Your Name"  className="outline-none w-80"/>
-          <button className="py-1 px-3 bg-[#0af034]" onClick={addTodohandle}>Add</button>
+          <input type="text" value={addtodo} onChange={(e)=>setAddtodo(e.target.value)} placeholder="Enter Your Name"  className="outline-none sm:w-80"/>
+          <button className="py-1 px-3 bg-[#0af034]" onClick={addTodohandle}>{editid ? 'Updata' : 'add'}</button>
         </div>
         <ul className="mt-3">
           {todo.map((v,index)=>{
@@ -48,7 +61,7 @@ setTodo(todo.filter((v)=>v.id !== id));
             <div className="flex justify-between ">
               <h1 className="py-1 text-lg">{v.text}</h1>
               <div className="gap-1 flex">
-                <button className="py-1 px-3 bg-[#d1ce24]">Edit</button><button className="py-1 px-3 bg-[#f00a0a]" onClick={()=>DeleteTodo(v.id)}>Delete</button>
+                <button className="py-1 px-3 bg-[#d1ce24]"onClick={()=>EditTodo(v.id)}>Edit</button><button className="py-1 px-3 bg-[#f00a0a]" onClick={()=>DeleteTodo(v.id)}>Delete</button>
               </div>
             </div>
           </li>)
